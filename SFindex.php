e88c5d8969c9d6f7005bb399138edc0bcdcb31b9
Sharon Freystaetter <47903703+sharonfrey@users.noreply.github.com>
2019-09-13 23:22:50 -0400

    Added bio for Sharon

---
+++ SFindex.php
@@ -0,0 +1,135 @@
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="UTF-8">
+		<title>CEN 4010 &bull; Fall 2019 &bull; Team 3</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Google+Sans|Roboto:100,300,400,500,700,900,100i,300i,400i,500i,700i,900i">
+		<link rel="stylesheet" href="stylesheet.css">
+	</head>
+	<body>
+		<nav class="navbar sticky-top navbar-expand-lg navbar-light shadow-sm" style="background-color: white;">
+			<a class="navbar-brand" href="#about-us"><i class="fas fa-heart text-danger"></i> <i class="fas fa-laptop-code"></i> Team 3</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="#about-us">About</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#team">Team</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#credit">Credit</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<div class="container">
+			<div class="text-left">
+				<span class="display-4">
+					<i><small><span class="text-muted">Team 3:</span></small>
+					<strong>Code Nxt</strong></i>
+				</span>
+			</div>
+			<h1 id="about-us">About Us</h1>
+			<p>Welcome to the website for Team 3! (a.k.a. team <strong>Code Nxt</strong>)</p>
+			<p>We have a diverse team with a variety of experience in software engineering. Our team is distributed across multiple cities, and we are using online collaboration tools&mdash;like Trello&mdash;to collaborate effectively.</p>
+			<p>Click the button below to visit the Github respository for our class project.</p>
+			<a href="https://github.com/orgs/cen4010-s2019-g03/" class="btn btn-primary"><i class="fab fa-github"></i> GitHub repo</a>
+			<hr />
+			<h1 id="team">Our Team</h1>
+			<div class="row mb-2">
+				<div class="col-md-6">
+					<div class="card flex-md-row mb-4 box-shadow h-md-250" id="jeff-campbell">
+						<div class="card-body d-flex flex-column align-items-start">
+							<h3>
+								Jeff<br />Campbell
+							</h3>
+							<p>&nbsp;</p>
+							<a href="http://lamp.eng.fau.edu/~jeffcampbell2016/m0">Visit page</a>
+						</div>
+						<img height="200" width="200" class="card-img-right flex-auto d-none d-md-block" src="assets/jc_pic_1.jpg" alt="Card image cap">
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="card flex-md-row mb-4 box-shadow h-md-250" id="sharon-freystaetter">
+						<div class="card-body d-flex flex-column align-items-start">
+							<h3>
+								Sharon<br />Freystaetter
+							</h3>
+							<p>&nbsp;</p>
+							<a href="http://lamp.eng.fau.edu/~sfreystaette2015/bio">&nbsp;</a>
+						</div>
+						<img class="invisible card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x200?theme=thumb" alt="Card image cap">
+					</div>
+				</div>
+			</div>
+			<div class="row mb-2">
+				<div class="col-md-6">
+					<div class="card flex-md-row mb-4 box-shadow h-md-250" id="francois-joseph">
+						<div class="card-body d-flex flex-column align-items-start">
+							<h3>
+								Francois<br />Joseph
+							</h3>
+							<p>&nbsp;</p>
+							<a href="#">&nbsp;</a>
+						</div>
+						<img class="invisible card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x200?theme=thumb" alt="Card image cap">
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="card flex-md-row mb-4 box-shadow h-md-250" id="kevin-lewitzke">
+						<div class="card-body d-flex flex-column align-items-start">
+							<h3>
+								Kevin<br />Lewitzke
+							</h3>
+							<p>&nbsp;</p>
+							<a href="http://lamp.eng.fau.edu/~klewitzke2017/kl_about.php">Visit page</a>
+						</div>
+						<img class="card-img-right flex-auto d-none d-md-block" src="assets/kl_pic_1.jpg" width="200" height="200" alt="Card image cap">
+					</div>
+				</div>
+			</div>
+			<hr />
+			<h1 id="credit">Credit</h1>
+			<p>The following resources were used to create this page:<br />
+			<a href="https://jquery.com/">jQuery</a><br />
+			<a href="https://getbootstrap.com/">Bootstrap</a><br />
+			<a href="https://fontawesome.com/">Font Awesome</a><br />
+			<a href="https://fonts.google.com/">Google Fonts</a><br />
+			<p>The scrolling effect for this page was created using code snippets from these sites:<br />
+			<a href="https://www.abeautifulsite.net/smoothly-scroll-to-an-element-without-a-jquery-plugin-2">A Beautiful Site (Cory LaViska)</a><br />
+			<a href="https://stackoverflow.com/a/14129990">Stack Overflow</a></p>
+		</div>
+		<!-- JQuery -->
+		<script	src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+		<script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/holder.min.js"></script>
+		<script src="https://kit.fontawesome.com/745dd24e30.js"></script>
+		<script>
+		Holder.addTheme('thumb', {
+			bg: '#55595c',
+			fg: '#eceeef',
+			text: 'Thumbnail'
+		});
+		</script>
+		<script>
+		$('a[href^="#"]').on('click', function(event) {
+			var target = $(this.getAttribute('href'));
+			if(target.length > 0) {
+				event.preventDefault();
+				if($(this.hash).offset()) {
+					var offset = $(this.hash).offset().top;
+				}
+				else {
+					var offset = 0;
+				}
+				$('html, body').animate({scrollTop: offset - 56}, 300);
+			}
+		});
+		</script>
+	</body>
+</html>
