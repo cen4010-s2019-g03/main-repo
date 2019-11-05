85f1fa2c7cc9efe7257b991ad92f0c010480bff4
Kevin Lewitzke <klewitzke2017@fau.edu>
2019-11-04 22:40:20 -0500

    vertical software prototype
    uploading all files which are part of the vertical software prototype (milestone 3)

---
+++ login.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		
+		<!-- Site CSS -->
+		<link rel="stylesheet" href="campusLive.css">
+		
+		<title>campusLive</title>
+	</head>
+	<body>
+		<h1>Login</h1>
+		<div class="col-sm-6 offset-sm-3">
+			<form id="loginForm" action="dashboard.php" method="POST">
+				<div class="form-group">
+					<label for="username">Enter username</label>
+					<input type="text" class="form-control" name="username" id="username">
+				</div>
+				<div class="form-group">
+					<label for="password">Enter password</label>
+					<input type="password" class="form-control" name="password" id="password">
+				</div>
+				<button type="submit" class="btn btn-primary" name="submit" id="submit">Login</button>
+			</form>
+		</div>
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+		<script src="https://kit.fontawesome.com/745dd24e30.js"></script>
+	</body>
+</html>
